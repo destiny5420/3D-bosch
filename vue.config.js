82b29f989fd1e450fb94cfe9f548730caf92d094
paper.window <destiny5420@gmail.com>
2020-09-09 17:44:30 +0800

    [Add] build product

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/3D-bosch/" : "/",
   outputDir: "docs"
 };
