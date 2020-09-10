4923779148bd894b1f4f7d4bcf8e66d418ed3011
paper.window <destiny5420@gmail.com>
2020-09-10 10:35:16 +0800

    [Add] 安裝vue router / vuex / vue axios package

--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
 import Vue from "vue";
 import App from "@/views/vApp/index.vue";
 
+// import vuex
+import Vuex from "vuex";
+Vue.use(Vuex);
+
+// import vue router
+import VueRouter from "vue-router";
+Vue.use(VueRouter);
+
+// import vue axios
+import axios from "axios";
+import VueAxios from "vue-axios";
+Vue.use(VueAxios, axios);
+
 import * as THREE from "three";
 Vue.use({
   install(Vue) {
