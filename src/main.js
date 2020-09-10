58b311810966971666660bfde455937c87041cb6
paper.window <destiny5420@gmail.com>
2020-09-10 11:01:30 +0800

    [Add] 加入vue router [Add] 加入vuex

--- src/main.js
+++ src/main.js
@@ -1,19 +1,18 @@
 import Vue from "vue";
 import App from "@/views/vApp/index.vue";
-
-// import vuex
-import Vuex from "vuex";
-Vue.use(Vuex);
-
-// import vue router
-import VueRouter from "vue-router";
-Vue.use(VueRouter);
+import router from "@/router"; // vue-router instance
+import store from "@/store"; // vuex store instance
 
 // import vue axios
 import axios from "axios";
 import VueAxios from "vue-axios";
 Vue.use(VueAxios, axios);
 
+// import vue-router-sync
+import { sync } from "vuex-router-sync";
+sync(store, router);
+
+// import three
 import * as THREE from "three";
 Vue.use({
   install(Vue) {
@@ -24,5 +23,7 @@ Vue.use({
 Vue.config.productionTip = false;
 
 new Vue({
+  router,
+  store,
   render: h => h(App)
 }).$mount("#app");
---
+++ src/store/index.js
@@ -0,0 +1,14 @@
+import Vue from "vue";
+import Vuex from "vuex";
+// import axios from "axios";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  strict: true,
+  state: {},
+  getters: {},
+  mutations: {},
+  actions: {},
+  modules: {}
+});
