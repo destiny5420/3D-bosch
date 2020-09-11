0ef944f7490f4aeccc82c0aa54ff59adf4e911eb
paper.window <destiny5420@gmail.com>
2020-09-11 10:42:31 +0800

    [Modify] Build project

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,4 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === "production" ? "/3D-bosch/" : "/",
-  outputDir: "docs",
-  chainWebpack: config => {
-    config.resolve.alias.set("style", resolve("src/common/style"));
-  }
+  outputDir: "docs"
 };
